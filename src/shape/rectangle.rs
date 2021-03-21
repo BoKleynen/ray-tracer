bd1336fe09d987999a69ee4951d787582935547d
Bo Kleynen <kleynenbo@gmail.com>
2021-03-21 18:58:27 +0100

    Fix rebase

--- src/shape/rectangle.rs
+++ src/shape/rectangle.rs
@@ -1,5 +1,5 @@
-use crate::math::{Ray, Transformation};
-use crate::shape::{Hit, Shape};
+use crate::math::Ray;
+use crate::shape::{Hit, Shape, AABB};
 use crate::K_EPSILON;
 use nalgebra::{Point3, Unit, Vector3};
 
@@ -8,32 +8,17 @@ pub struct Rectangle {
     a: Vector3<f64>,
     b: Vector3<f64>,
     normal: Unit<Vector3<f64>>,
-    transformation: Transformation,
-}
-
-impl Rectangle {
-    fn shading_normal(&self, normal: &Vector3<f64>) -> Vector3<f64> {
-        self.transformation
-            .inverse()
-            .matrix()
-            .transpose()
-            .transform_vector(normal)
-            .normalize()
-    }
 }
 
 impl Shape for Rectangle {
     fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        let inv_ray = self.transformation.apply_inverse(ray);
-
-        let t =
-            (self.p - inv_ray.origin()).dot(&self.normal) / inv_ray.direction().dot(&self.normal);
+        let t = (self.p - ray.origin()).dot(&self.normal) / ray.direction().dot(&self.normal);
 
         if t <= K_EPSILON {
             return None;
         }
 
-        let q = inv_ray.origin() + t * inv_ray.direction();
+        let q = ray.origin() + t * ray.direction();
         let d = q - self.p;
 
         let ddota = d.dot(&self.a);
@@ -48,12 +33,19 @@ impl Shape for Rectangle {
 
         Some(Hit {
             t,
-            normal: self.shading_normal(&self.normal),
+            normal: *self.normal,
             local_hit_point: q,
         })
     }
 
-    fn count_intersection_tests(&self, ray: &Ray) -> usize {
+    fn count_intersection_tests(&self, _ray: &Ray) -> usize {
         1
+    }
+
+    fn bbox(&self) -> AABB {
+        let p0 = self.p;
+        let p1 = self.p + self.a + self.b;
+
+        AABB::new(p0, p1)
     }
 }
