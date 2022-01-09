e5dbaae9821a056fa98dfe025d8f01213b69ac03
Bo Kleynen <kleynenbo@gmail.com>
2022-01-09 22:34:31 +0100

    Cleanup old files

--- src/film/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use frame_buffer::FrameBuffer;
-pub use pixel::Pixel;
-pub use rgb::Rgb;
-
-mod frame_buffer;
-mod pixel;
-mod rgb;
--- src/math/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use orthonormal_basis::OrthonormalBasis;
-pub use ray::Ray;
-pub use transformation::{Transformable, Transformation};
-
-mod orthonormal_basis;
-mod ray;
-mod transformation;
