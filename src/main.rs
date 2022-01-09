1b7bb86ae2ec0c7e3741cdcf9c95d4c6b02d6cd3
Bo Kleynen <kleynenbo@gmail.com>
2022-01-09 22:42:21 +0100

    Add main function outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use clap::Parser;
+use std::num::NonZeroU8;
+use tracing::Level;
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Cli {
+    filenames: Vec<String>,
+
+    /// Use specified number of threads for rendering.
+    #[clap(long, value_name = "num")]
+    nthreads: Option<NonZeroU8>,
+
+    /// Write the final image to the given filename.
+    #[clap(long, value_name = "FILE")]
+    outfile: Option<String>,
+
+    /// Automatically reduce a number of quality settings to
+    /// render more quickly.
+    #[clap(long)]
+    quick: bool,
+
+    /// Suppress all text output other than error messages.
+    #[clap(long)]
+    quiet: bool,
+
+    /// Sets the log level, possible values are:
+    /// TRACE, DEBUG, INFO, WARN, ERROR.
+    /// Default value is INFO
+    #[clap(long)]
+    log_level: Option<String>,
+}
+
 fn main() {
-    println!("hello world")
+    let cli: Cli = Cli::parse();
+
+    if !cli.quiet {
+        tracing_subscriber::fmt()
+            .with_max_level(
+                cli.log_level
+                    .unwrap_or_default()
+                    .parse::<Level>()
+                    .unwrap_or(Level::INFO),
+            )
+            .init();
+    }
+
+    println!("{:?}", cli.filenames)
 }
