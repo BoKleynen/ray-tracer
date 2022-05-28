3840d3074d0bf0eb38b6eab1782a974083fa5e68
Bo Kleynen <kleynenbo@gmail.com>
2022-05-28 21:42:37 +0200

    Add shape skeletons

--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -1,5 +1,15 @@
+mod cone;
+mod cylinder;
+mod disk;
+mod hyperboloid;
+mod paraboloid;
 mod sphere;
 mod transformed;
 
+pub use cone::Cone;
+pub use cylinder::Cylinder;
+pub use disk::Disk;
+pub use hyperboloid::Hyperboloid;
+pub use paraboloid::Paraboloid;
 pub use sphere::Sphere;
 pub use transformed::Transformed;
